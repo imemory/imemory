50db3e608089d54417ab3e11f2000ba152c43932
tarcisio <sassara.tarcisio@gmail.com>
2010-05-19 18:13:03 -0300

    alterado os hashs

--- app/config/core.php
+++ app/config/core.php
@@ -159,7 +159,7 @@
 /**
  * The name of CakePHP's session cookie.
  */
-	Configure::write('Session.cookie', 'CAKEPHP');
+	Configure::write('Session.cookie', 'imemory');
 
 /**
  * Session time out time (in seconds).
@@ -195,12 +195,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '0FgaC9mDYhG93b0qyJfIxfs2guVoUubWwvniR2Gi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '72496749683668593096574535445');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
