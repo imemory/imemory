17ca6585f702d70ff6107363e5b1d7c6c0c02011
tarcisio <sassara.tarcisio@gmail.com>
2010-05-19 18:38:29 -0300

    feito a chamada para pegar os ultimos usuários cadastrados

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -6,6 +6,9 @@ class UsersController extends AppController
 	//--------------------------------------------------------------------------
 	public function index()
 	{
+		$latest_users = $this->User->getLatest();
+		$this->set('latest_users', $latest_users);
 	}
 	
 }
+
---
+++ app/views/users/index.ctp
@@ -0,0 +1,4 @@
+<?php
+
+pr($latest_users);
+
