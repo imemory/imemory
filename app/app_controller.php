f000664d30ecd70e9b0ffaeee9f5c2bc05096fde
tarcisio <sassara.tarcisio@gmail.com>
2010-05-23 09:44:12 -0300

    adicionado documentação no app_controller

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,10 +3,28 @@
 class AppController extends Controller
 {
 	//--------------------------------------------------------------------------
-	public $components = array('Auth', 'Session');
+	/**
+	 * Componentes do CakePHP usados
+	 * Auth: Usado para criar a autenticação do usuário, faz o login e o logout
+	 * do usuário.
+	 *
+	 * RequestHandler: Usado para obter mais informações sobre as requisições
+	 * dos usuários como por exemplo: se foi um método POST.
+	 *
+	 * Session: Controla a sessão.
+	*/
+	public $components = array('Auth', 'RequestHandler', 'Session');
 	
 	
 	//--------------------------------------------------------------------------
+	/**
+	 * Helpers usados
+	 * Session: Usado para trabalhar com a sessão nas views
+	 *
+	 * Html: Usado para criar elementos do HTML nas views
+	 *
+	 * Form: Para criar elementos de formulários nas views
+	 */
 	public $helpers = array('Session', 'Html', 'Form');
 }
 
