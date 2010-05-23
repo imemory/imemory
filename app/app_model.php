8be7e1c4ee6a0894d11638015fafc0e155074e40
tarcisio <sassara.tarcisio@gmail.com>
2010-05-23 09:46:47 -0300

    adicionado documentação no app_model

--- app/app_model.php
+++ app/app_model.php
@@ -3,10 +3,20 @@
 class AppModel extends Model
 {
 	//--------------------------------------------------------------------------
+	/**
+	 * Por padrão, não faz buscas recursivas nos models.
+	 *
+	 * Isso é uma grande melhoria pois o cakephp automaticamente faz as queries
+	 * para retornar as associações entre os models. Desta forma, é preciso
+	 * especificar manualmente o que o Model deve retornar.
+	 */
 	public $recursive = -1;
 	
 	
 	//--------------------------------------------------------------------------
+	/**
+	 * Por padrão todos os Models terão o behavior Containable.
+	 */
 	public $actsAs = array('Containable');
 }
 
