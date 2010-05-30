5090f16ebcfeccbeff632b007d8e072b7b502fb1
tarcisio <sassara.tarcisio@gmail.com>
2010-05-29 23:37:09 -0300

    adicionado os metodos basicos do google tracker

--- app/views/helpers/google.php
+++ app/views/helpers/google.php
@@ -4,5 +4,37 @@ class GoogleHelper extends AppHelper
 {
 	//--------------------------------------------------------------------------
 	public $helpers = array('Html');
+	
+	
+	//--------------------------------------------------------------------------
+	public function initTracker()
+	{
+		$trackCode = Configure::read('Google.tracking');
+		
+		$script  = "
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '{$trackCode}']);
+		_gaq.push(['_trackPageview']);";
+		
+		return $this->Html->scriptBlock($script, array('inline' => true));
+	}
+	
+	
+	//--------------------------------------------------------------------------
+	public function endTracker()
+	{
+		if (Configure::read('debug') > 0) {
+			return;
+		}
+		
+		$script = "
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();";
+		
+		return $this->Html->scriptBlock($script, array('inline' => true));
+	}
 }
 
