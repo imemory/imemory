5b298a7a9eaf13eabdeff18ddf894a0f6d152f5b
tarcisio <sassara.tarcisio@gmail.com>
2010-08-08 23:21:42 -0300

    redirecionado para a pagina do grupo que recebeu a mensagem

--- app/controllers/group_messages_controller.php
+++ app/controllers/group_messages_controller.php
@@ -48,12 +48,15 @@ class GroupMessagesController extends AppController
 	public function add()
 	{
 		if ( ! empty($this->data)) {
-			
 			$this->data['GroupMessage']['user_id'] = $this->Auth->user('id');
 			
 			if ($this->GroupMessage->save($this->data)) {
 				$this->Session->setFlash('Mensagem enviada');
-				$this->redirect(array('controller' => 'home'));
+				$this->redirect(array(
+				    'controller' => 'groups',
+				    'action' => 'view',
+				    $this->data['GroupMessage']['group_id']
+                ));
 			}
 		}
 	}
