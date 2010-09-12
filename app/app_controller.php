f7e96a0ade8bbe0a95f939e37a8b5c3c8396f27c
tarcisio <sassara.tarcisio@gmail.com>
2010-09-12 17:52:38 -0300

    corrigido espaço nos comentarios

--- app/app_controller.php
+++ app/app_controller.php
@@ -4,59 +4,59 @@ class AppController extends Controller
 {
     //--------------------------------------------------------------------------
     /**
-    * Componentes do CakePHP usados
-    * Auth: Usado para criar a autenticação do usuário, faz o login e o logout
-    * do usuário.
-    *
-    * Log: Usado para logar ações dos usuários
-    *
-    * P28n: Usado para alterar a linguagem do sistema
-    *
-    * RequestHandler: Usado para obter mais informações sobre as requisições
-    * dos usuários como por exemplo: se foi um método POST.
-    *
-    * Session: Controla a sessão.
-    */
+     * Componentes do CakePHP usados
+     * Auth: Usado para criar a autenticação do usuário, faz o login e o logout
+     * do usuário.
+     *
+     * Log: Usado para logar ações dos usuários
+     *
+     * P28n: Usado para alterar a linguagem do sistema
+     *
+     * RequestHandler: Usado para obter mais informações sobre as requisições
+     * dos usuários como por exemplo: se foi um método POST.
+     *
+     * Session: Controla a sessão.
+     */
     public $components = array('Auth', 'Log', 'P28n', 'RequestHandler', 'Session');
     
     
     //--------------------------------------------------------------------------
     /**
-    * Helpers usados
-    *
-    * Form: Para criar elementos de formulários nas views
-    *
-    * Gravatar: Interface para as carinhas do gravatar
-    *
-    * Google: Implementa vários serviços do Google como o analytics
-    *
-    * Html: Usado para criar elementos do HTML nas views
-    *
-    * Session: Usado para trabalhar com a sessão nas views
-    *
-    * Text: Funções para manipular texto. Por exemplo truncar o texto
-    *
-    * Time: Para converter datas e horas em formatos agradaveis
-    *
-    */
+     * Helpers usados
+     *
+     * Form: Para criar elementos de formulários nas views
+     *
+     * Gravatar: Interface para as carinhas do gravatar
+     *
+     * Google: Implementa vários serviços do Google como o analytics
+     *
+     * Html: Usado para criar elementos do HTML nas views
+     *
+     * Session: Usado para trabalhar com a sessão nas views
+     *
+     * Text: Funções para manipular texto. Por exemplo truncar o texto
+     *
+     * Time: Para converter datas e horas em formatos agradaveis
+     *
+     */
     public $helpers = array('Form', 'Gravatar', 'Google', 'Html', 'Session', 'Text', 'Time');
     
     
     //--------------------------------------------------------------------------
     /**
-    *
-    * Mantem uma referência para o usuário logado
-    *
-    */
+     *
+     * Mantem uma referência para o usuário logado
+     *
+     */
     public $currentUser;
     
     
     //--------------------------------------------------------------------------
     /**
-    * Antes de executar a ação
-    * 
-    * Passa para a view o usuário logado atualmente
-    */
+     * Antes de executar a ação
+     * 
+     * Passa para a view o usuário logado atualmente
+     */
     public function beforeFilter()
     {
         parent::beforeFilter();
@@ -68,10 +68,10 @@ class AppController extends Controller
 
     //--------------------------------------------------------------------------
     /**
-    *
-    * Configura o AuthComponent
-    *
-    */
+     *
+     * Configura o AuthComponent
+     *
+     */
     public function initAuth()
     {
         $this->Auth->loginRedirect  = array('controller' => 'home', 'action' => 'index');
@@ -83,10 +83,10 @@ class AppController extends Controller
     
     //--------------------------------------------------------------------------
     /**
-    *
-    * Define o atributo login contendo os dados do usuário corrente
-    *
-    */
+     *
+     * Define o atributo login contendo os dados do usuário corrente
+     *
+     */
     public function initLogin()
     {
         $user = $this->Auth->user();
@@ -100,10 +100,10 @@ class AppController extends Controller
     
     
     /**
-    *
-    * Define a linguagem de acordo com a preferencia do usuário
-    *
-    */
+     *
+     * Define a linguagem de acordo com a preferencia do usuário
+     *
+     */
     public function initLang()
     {
         if($this->currentUser) {
--- app/app_model.php
+++ app/app_model.php
@@ -2,21 +2,21 @@
 
 class AppModel extends Model
 {
-	//--------------------------------------------------------------------------
-	/**
-	 * Por padrão, não faz buscas recursivas nos models.
-	 *
-	 * Isso é uma grande melhoria pois o cakephp automaticamente faz as queries
-	 * para retornar as associações entre os models. Desta forma, é preciso
-	 * especificar manualmente o que o Model deve retornar.
-	 */
-	public $recursive = -1;
-	
-	
-	//--------------------------------------------------------------------------
-	/**
-	 * Por padrão todos os Models terão o behavior Containable.
-	 */
-	public $actsAs = array('Containable');
+    //--------------------------------------------------------------------------
+    /**
+     * Por padrão, não faz buscas recursivas nos models.
+     *
+     * Isso é uma grande melhoria pois o cakephp automaticamente faz as queries
+     * para retornar as associações entre os models. Desta forma, é preciso
+     * especificar manualmente o que o Model deve retornar.
+     */
+    public $recursive = -1;
+    
+    
+    //--------------------------------------------------------------------------
+    /**
+     * Por padrão todos os Models terão o behavior Containable.
+     */
+    public $actsAs = array('Containable');
 }
 
