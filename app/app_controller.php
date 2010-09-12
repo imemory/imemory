2ab092795e1932fb31be2b012514c0ec49d96740
tarcisio <sassara.tarcisio@gmail.com>
2010-09-12 17:50:02 -0300

    adicionado metodo initAuth que configura o component auth

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,98 +2,113 @@
 
 class AppController extends Controller
 {
-	//--------------------------------------------------------------------------
-	/**
-	 * Componentes do CakePHP usados
-	 * Auth: Usado para criar a autenticação do usuário, faz o login e o logout
-	 * do usuário.
-	 *
-	 * Log: Usado para logar ações dos usuários
-	 *
-	 * P28n: Usado para alterar a linguagem do sistema
-	 *
-	 * RequestHandler: Usado para obter mais informações sobre as requisições
-	 * dos usuários como por exemplo: se foi um método POST.
-	 *
-	 * Session: Controla a sessão.
-	*/
-	public $components = array('Auth', 'Log', 'P28n', 'RequestHandler', 'Session');
-	
-	
-	//--------------------------------------------------------------------------
-	/**
-	 * Helpers usados
-	 *
-	 * Form: Para criar elementos de formulários nas views
-	 *
-	 * Gravatar: Interface para as carinhas do gravatar
-	 *
-	 * Google: Implementa vários serviços do Google como o analytics
-	 *
-	 * Html: Usado para criar elementos do HTML nas views
-	 *
-	 * Session: Usado para trabalhar com a sessão nas views
-	 *
-	 * Text: Funções para manipular texto. Por exemplo truncar o texto
-	 *
-	 * Time: Para converter datas e horas em formatos agradaveis
-	 *
-	 */
-	public $helpers = array('Form', 'Gravatar', 'Google', 'Html', 'Session', 'Text', 'Time');
-	
-	
-	//--------------------------------------------------------------------------
-	/**
-	 *
-	 * Mantem uma referência para o usuário logado
-	 *
-	*/
-	public $currentUser;
-	
-	
-	//--------------------------------------------------------------------------
-	/**
-	 * Antes de executar a ação
-	 * 
-	 * Passa para a view o usuário logado atualmente
-	 */
-	 public function beforeFilter()
-	 {
-	    parent::beforeFilter();
-	    
-	    $this->initLogin();
-	    $this->initLang();
-	 }
-	 
-	 
-	 //--------------------------------------------------------------------------
-	 /**
-	  *
-	  * Define o atributo login contendo os dados do usuário corrente
-	  *
-	 */
-	 public function initLogin()
-	 {
-	    $user = $this->Auth->user();
-	    
-	    if ( ! empty($user)) {
-	        unset($user['User']['password']);
-	        $this->currentUser = $user['User'];
-    	    $this->set('User', $user);
-	    }
-	 }
-	 
-	 
-	 /**
-	  *
-	  * Define a linguagem de acordo com a preferencia do usuário
-	  *
-	  */
-	 public function initLang()
-	 {
-	    if($this->currentUser) {
-	        $this->P28n->change($this->currentUser['language']);
-	    }
-	 }
+    //--------------------------------------------------------------------------
+    /**
+    * Componentes do CakePHP usados
+    * Auth: Usado para criar a autenticação do usuário, faz o login e o logout
+    * do usuário.
+    *
+    * Log: Usado para logar ações dos usuários
+    *
+    * P28n: Usado para alterar a linguagem do sistema
+    *
+    * RequestHandler: Usado para obter mais informações sobre as requisições
+    * dos usuários como por exemplo: se foi um método POST.
+    *
+    * Session: Controla a sessão.
+    */
+    public $components = array('Auth', 'Log', 'P28n', 'RequestHandler', 'Session');
+    
+    
+    //--------------------------------------------------------------------------
+    /**
+    * Helpers usados
+    *
+    * Form: Para criar elementos de formulários nas views
+    *
+    * Gravatar: Interface para as carinhas do gravatar
+    *
+    * Google: Implementa vários serviços do Google como o analytics
+    *
+    * Html: Usado para criar elementos do HTML nas views
+    *
+    * Session: Usado para trabalhar com a sessão nas views
+    *
+    * Text: Funções para manipular texto. Por exemplo truncar o texto
+    *
+    * Time: Para converter datas e horas em formatos agradaveis
+    *
+    */
+    public $helpers = array('Form', 'Gravatar', 'Google', 'Html', 'Session', 'Text', 'Time');
+    
+    
+    //--------------------------------------------------------------------------
+    /**
+    *
+    * Mantem uma referência para o usuário logado
+    *
+    */
+    public $currentUser;
+    
+    
+    //--------------------------------------------------------------------------
+    /**
+    * Antes de executar a ação
+    * 
+    * Passa para a view o usuário logado atualmente
+    */
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+        
+        $this->initAuth();
+        $this->initLogin();
+        $this->initLang();
+    }
+
+    //--------------------------------------------------------------------------
+    /**
+    *
+    * Configura o AuthComponent
+    *
+    */
+    public function initAuth()
+    {
+        $this->Auth->loginRedirect  = array('controller' => 'home', 'action' => 'index');
+        $this->Auth->logoutRedirect = array('controller' => 'home', 'action' => 'index');
+        $this->Auth->loginError = __('Ops! Login failed. Invalid username or password.', true);
+        $this->Auth->authError  = __('Ops! You must be logged in to do this.', true);
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    /**
+    *
+    * Define o atributo login contendo os dados do usuário corrente
+    *
+    */
+    public function initLogin()
+    {
+        $user = $this->Auth->user();
+        
+        if ( ! empty($user)) {
+            unset($user['User']['password']);
+            $this->currentUser = $user['User'];
+            $this->set('User', $user);
+        }
+    }
+    
+    
+    /**
+    *
+    * Define a linguagem de acordo com a preferencia do usuário
+    *
+    */
+    public function initLang()
+    {
+        if($this->currentUser) {
+            $this->P28n->change($this->currentUser['language']);
+        }
+    }
 }
 
