f496344d65e8d53f74275dd1cb88ce8fe5254be9
tarcisio <sassara.tarcisio@gmail.com>
2010-09-23 00:27:34 -0300

    quando adicionado flashcard no grupo, todos os usuários recebem o flashcard

--- app/models/flashcards_group.php
+++ app/models/flashcards_group.php
@@ -12,5 +12,26 @@ class FlashcardsGroup extends AppModel
         )
     );
     
-    
+    public function afterSave($created = false)
+    {
+        if ( ! $created) {
+            return;
+        }
+        
+        $group_id     = $this->data['FlashcardsGroup']['group_id'];
+        $flashcard_id = $this->data['FlashcardsGroup']['flashcard_id'];
+        
+        $sql = "
+        insert into flashcards_users(flashcard_id, user_id, created, updated)
+        select {$flashcard_id} as flashcard_id, user_id, now(), now()
+        from   memberships
+        where  group_id = {$group_id}
+        and    user_id not in (
+                 select user_id
+                 from flashcards_users
+                 where flashcard_id = {$flashcard_id}
+               )
+        ";
+        $this->query($sql);
+    }
 }
