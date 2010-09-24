b41d575da698c4c3b75e8191bfc316e74168a915
tarcisio <sassara.tarcisio@gmail.com>
2010-09-24 18:37:53 -0300

    atualizado o helper do google analytics

--- app/views/helpers/google.php
+++ app/views/helpers/google.php
@@ -9,6 +9,10 @@ class GoogleHelper extends AppHelper
 	//--------------------------------------------------------------------------
 	public function initTracker()
 	{
+	    if (Configure::read('debug') > 0) {
+			return;
+		}
+		
 		$trackCode = Configure::read('Google.tracking');
 		
 		$script  = "
@@ -28,11 +32,11 @@ class GoogleHelper extends AppHelper
 		}
 		
 		$script = "
-		(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();";
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();";
 		
 		return $this->Html->scriptBlock($script, array('inline' => true));
 	}
